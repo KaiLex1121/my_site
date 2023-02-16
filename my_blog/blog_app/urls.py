0bba9668b714553eb7747761c11b803a81c7347d
Lesha Kai <kojjmin@gmail.com>
2023-02-16 16:18:37 +0300

    New view and template were created

--- my_blog/blog_app/urls.py
+++ my_blog/blog_app/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 
 urlpatterns = [
+    path('kianu_rivz/', views.show_kianu_rivz_post, name='kianu'),
+    path('guinnes_records/', views.show_guinness_world_records, name='guinnes_records'),
     path('<int:post_number>/', views.show_post_by_number),
     path('<str:post_name>/', views.show_post_by_name),
     path('', views.show_blogpage),
--- my_blog/blog_app/views.py
+++ my_blog/blog_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template.loader import render_to_string
+from django.urls import reverse
 
 
 def show_mainpage(request):
@@ -14,14 +15,42 @@ def show_blogpage(request):
 
 def show_post_by_name(request, post_name):
 
+    post_name = post_name.lower()
+
+    response = None
+
+    if post_name == 'киану_ривз':
+        response = reverse('kianu')
+
+    elif post_name == 'рекорды_гиннеса':
+        response = reverse('guinnes_records')
+
+    if response is not None:
+        return HttpResponseRedirect(response)
+
+    return HttpResponse(f'Поста {post_name} пока не существует')
+
+
+def show_guinness_world_records(request):
+
+    context = {
+        'power_man': 'Narve Laeret',
+        'bar_name': "Bob’s BBQ & Grill",
+        'count_needle': 1790,
+    }
+
+    return render(request, 'blog_app/guinnes_records.html', context=context)
+
+
+def show_kianu_rivz_post(request):
+
     context_data = {
         'year_born': 1964,
         'city_born': 'Бейрут',
         'movie_name': 'Матрица'
     }
 
-    if post_name.lower() == 'киану_ривз':
-        return render(request, 'blog_app/kianu_rivz_post.html', context=context_data)
+    return render(request, 'blog_app/kianu_rivz_post.html', context=context_data)
 
 
 def show_post_by_number(request, post_number):
