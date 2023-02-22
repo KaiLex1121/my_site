614e73772b354f79602fdc81c2f83bb015dcb490
Lesha Kai <kojjmin@gmail.com>
2023-02-22 15:33:32 +0300

    Added template inheritance, refactoring related views

--- my_blog/blog_app/views.py
+++ my_blog/blog_app/views.py
@@ -2,15 +2,78 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template.loader import render_to_string
 from django.urls import reverse
+from random import sample
+
+
+def get_posts():
+
+    posts = [
+        {
+            'title': 'Рыбалка',
+            'description': 'Хорошо посидели',
+            'date': '21 авг 2021',
+            'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi distinctio doloremque et fuga iste neque, pariatur quod sit veritatis voluptates?'
+        },
+        {
+            'title': 'Париж',
+            'description': 'Незабываемое путешествие',
+            'date': '5 сент 2020',
+            'content': '''Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Commodi distinctio doloremque et fuga iste neque, pariatur quod sit veritatis voluptates?'''
+        },
+        {
+            'title': 'Финал лиги чемпионов',
+            'description': 'Реал опять выиграл ЛЧ',
+            'date': '28 мая 2022',
+            'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui, rem.'
+        },
+        {
+            'title': 'Охота на уток',
+            'description': 'Ни одна утка не пострадала',
+            'date': '7 дек 2019',
+            'content': 'Lorem ipsum dolor sit amet.'
+        },
+        {
+            'title': 'Фестиваль огурца',
+            'description': 'Суздаль ждет тебя',
+            'date': '12 июль 2021',
+            'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci consectetur id inventore odit recusandae!'
+        },
+        {
+            'title': 'Нашествие',
+            'description': 'Даешь рок, но в следующем году',
+            'date': '29 июль 2021',
+            'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est mollitia recusandae rem?'
+        },
+        {
+            'title': 'Новый год',
+            'description': 'Эх, еще один год пролетел',
+            'date': '31 дек 2022',
+            'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A architecto corporis fuga ipsam laboriosam, nesciunt non quae qui ut veniam.'
+        },
+    ]
+
+    return posts
 
 
 def show_mainpage(request):
-    response = render_to_string('blog_app/index.html')
-    return HttpResponse(response)
+
+    chosen_posts = sample(get_posts(), 3)
+
+    context_data = {
+        'posts': chosen_posts
+    }
+
+    return render(request, 'blog_app/index.html', context=context_data)
 
 
 def show_blogpage(request):
-    return render(request, 'blog_app/posts.html')
+
+    context_data = {
+        'posts': get_posts()
+    }
+
+    return render(request, 'blog_app/posts.html', context=context_data)
 
 
 def show_post_by_name(request, post_name):
